223edecc31ff0c8b6637bcc8ede049ae1bef477e
Diallo Ibrahima <ibrahimasory22@hotmail.com>
2018-03-14 21:57:26 +0100

    first commit

---
+++ Poligon/main.cpp
@@ -0,0 +1,22 @@
+// Poligon.cpp : Defines the entry point for the console application.
+//
+#include "stdafx.h"
+#include<iostream>
+#include"Poligon.h"
+using namespace std;
+
+int main()
+{
+	// Entrada: un polígon triangle
+	// Sortida: mostra el triangle, mostra un quadrat i el seu perímetre
+		cout<<"entra el punt del triangle";
+	Poligon pol; // un triangle
+	Punt2D p;
+	p.llegir();
+	pol.afegirPunt(p);
+	pol.mostra();
+	// el mètode perimetre() no està implementat
+	//cout << endl << "El perimetre es " << pol.perimetre() << endl;
+	return 0;
+}
+
